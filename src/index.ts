2db1155977f33d5fd60b743b0cfdacc04ed47e7e
Julius <juliushussl@gmail.com>
2021-08-23 00:39:45 +0200

    progress

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,9 @@ admin.initializeApp({
     "https://battlebots-30657-default-rtdb.europe-west1.firebasedatabase.app",
 });
 
-const maxUserOffTime = 10000;
+const maxUserOffTime = 3000;
 
-var userCheckInterval = 5000;
+var userCheckInterval = 2000;
 
 setInterval(() => {
   checkUser();
@@ -21,23 +21,55 @@ var lobbyRef = db.ref("lobbys");
 lobbyRef.on(
   "child_changed",
   (snapshot: any) => {
-    console.log(snapshot.val());
+    if (checkPlayerInLobby(snapshot.val())) {
+      checkAdminInLobby(snapshot.val());
+    }
   },
   (errorObject: any) => {
     console.log("The read failed: " + errorObject.name);
   }
 );
 
+function checkPlayerInLobby(lobby: any): boolean {
+  console.log(lobby);
+  if (!lobby.player) {
+    removeLobby(lobby.settings.id);
+    return false;
+  }
+  return true;
+}
+
+function checkAdminInLobby(lobby: any) {
+  let mappedPlayer = formatUserToMap(lobby.player);
+  let admin = lobby.adminUid;
+  console.log(admin);
+  console.log(lobby);
+  console.log(mappedPlayer);
+  if (!mappedPlayer.has(admin)) {
+    let newAdmin = getRandomKey(mappedPlayer);
+    console.log(newAdmin);
+    lobbyRef.child(lobby.settings.id).update({ adminUid: newAdmin });
+  }
+}
+
+function removeLobby(lobbyId: string) {
+  lobbyRef.child(lobbyId).remove();
+}
+
 var gamesRef = db.ref("games");
 gamesRef.on(
   "child_changed",
   (snapshot: any) => {
-    console.log(snapshot.val());
+    //  console.log(snapshot.val());
   },
   (errorObject: any) => {
     console.log("The read failed: " + errorObject.name);
   }
 );
+
+function gameHasLobby(game: any) {
+  // lobbyRef.child(game.)
+}
 
 function checkUser() {
   var userRef = db.ref("user");
@@ -52,7 +84,12 @@ function checkUser() {
 }
 
 function checkLobby(user: any) {
-  var lobbyIdRef = db.ref("lobby");
+  var lobbyIdRef = db.ref("lobbys/" + user.lobbyId);
+  lobbyIdRef.once("value", function (snapshot: any) {
+    if (!snapshot.val()) {
+      deleteUser(user);
+    }
+  });
 }
 
 function checkLastOnline(user: any) {
@@ -89,4 +126,10 @@ function formatUserToMap(obj: any): Map<string, any> {
     map.set(key, obj[key]);
   }
   return map;
+}
+
+// returns random key from Set or Map
+function getRandomKey(collection: any) {
+  let keys = Array.from(collection.keys());
+  return keys[Math.floor(Math.random() * keys.length)];
 }
